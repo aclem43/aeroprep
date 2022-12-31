e7afdd81eeb2ec7d414210d2e5778cdaeb4aa064
aclem43 <aclemeur@gmail.com>
2023-01-01 07:37:40 +1000

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.core.aeroprep',
+  appName: 'aeroprep',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
