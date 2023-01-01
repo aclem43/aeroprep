0f3888523a9c1758564dab0222b3eecfd28734b6
aclem43 <aclemeur@gmail.com>
2023-01-01 15:24:21 +1000

    Initial Progress

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,58 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+  import router from '@/router'
+
+  const goToPrep = () => {
+    router.push('prep')
+  }
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="group center">
+    <v-hover>
+      <template v-slot:default="{ isHovering, props }">
+        <v-sheet
+          v-bind="props"
+          rounded="xl"
+          class="button mx-auto prevent-select"
+          elevation="12"
+          :color="isHovering ? 'blue-lighten-3' : 'blue-lighten-2'"
+          @click="goToPrep()"
+        >
+          <strong>Prep</strong>
+        </v-sheet>
+      </template>
+    </v-hover>
+
+    <v-sheet rounded="xl" class="button mx-auto" color="green-lighten-3">
+    </v-sheet>
+  </div>
 </template>
+
+<style scoped>
+  .group {
+    display: flex;
+    align-items: center;
+    height: 100vh;
+  }
+
+  .button {
+    height: 10vw;
+    line-height: 10vw;
+    width: 10vw;
+    text-align: center;
+    cursor: pointer;
+  }
+  .buttonText {
+  }
+
+  .button :hover {
+  }
+
+  @media only screen and (max-width: 1000px) {
+    .button {
+      height: 100px;
+      line-height: 100px;
+      width: 100px;
+    }
+  }
+</style>
---
+++ src/views/PrepView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <div class="left">
+      <div class="leftTop"></div>
+      <div class="leftBottom"></div>
+    </div>
+    <div class="right">a</div>
+  </div>
+</template>
+
+<style>
+  .container {
+    display: flex;
+  }
+  .left {
+    flex-grow: 1;
+    background-color: aqua;
+  }
+
+  .leftTop {
+    background-color: beige;
+  }
+
+  .leftBottom {
+    background-color: bisque;
+  }
+  .right {
+    background-color: aquamarine;
+    width: 40%;
+  }
+</style>
