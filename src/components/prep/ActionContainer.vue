ae1449211fea88883e7fd7ed38f16a22c204e358
aclem43 <aclemeur@gmail.com>
2023-02-15 21:08:12 +1000

    Redid tab system

--- src/components/prep/ActionContainer.vue
+++ src/components/prep/ActionContainer.vue
@@ -1,52 +1,65 @@
 <script setup lang="ts">
-  import { getPage } from '@/scripts/prep/actionpage'
-  import FlightAction from './actions/FlightAction.vue'
-  import FuelAction from './actions/FuelAction.vue'
-  import NoteAction from './actions/NoteAction.vue'
-  import RiskAction from './actions/RiskAction.vue'
-  import WeatherAction from './actions/WeatherAction.vue'
+  import { getPage, getPages, type Page } from '@/scripts/prep/actionpage'
+  import { nextTick } from 'vue'
+
   const page = getPage()
+  const pages = getPages()
+
+  const addItem = (newPage: Page) => {
+    const removed = pages.pages.splice(0, 1)
+    pages.pages.push(
+      ...pages.morePages.splice(pages.morePages.indexOf(newPage), 1)
+    )
+    pages.morePages.push(...removed)
+    nextTick(() => {
+      page.value = newPage.shortName
+    })
+  }
 </script>
 
 <template>
   <v-card elevation="12">
-    <v-tabs v-model="page" bg-color="primary">
-      <v-tab value="flight"> Todays Flight </v-tab>
-      <v-tab value="weather">
-        <v-icon style="margin-right: 10px">mdi-cloud</v-icon>
-        Weather
+    <v-tabs v-model="page" fixed-tabs color="primary">
+      <v-tab v-for="tab in pages.pages" :key="tab.name" :value="tab.shortName">
+        <v-icon v-if="tab.icon"> {{ tab.icon }}</v-icon>
+        {{ tab.name }}
       </v-tab>
-      <v-tab value="fuel">
-        <v-icon style="margin-right: 10px">mdi-fuel</v-icon>
-        Fuel
-      </v-tab>
-      <v-tab value="risk">
-        <v-icon style="margin-right: 10px">
-          mdi-order-bool-ascending-variant
-        </v-icon>
-        Risk assessment
-      </v-tab>
-      <v-tab value="note">
-        <v-icon style="margin-right: 10px"> mdi-note </v-icon>
-        Notes
-      </v-tab>
+
+      <v-menu v-if="pages.morePages.length">
+        <template v-slot:activator="{ props }">
+          <v-btn
+            variant="plain"
+            rounded="0"
+            class="align-self-center me-4"
+            height="100%"
+            v-bind="props"
+          >
+            more
+            <v-icon end> mdi-menu-down </v-icon>
+          </v-btn>
+        </template>
+
+        <v-list class="bg-grey-lighten-3">
+          <v-list-item
+            v-for="tab in pages.morePages"
+            :key="tab.name"
+            @click="addItem(tab)"
+          >
+            <v-icon v-if="tab.icon"> {{ tab.icon }}</v-icon>
+            {{ tab.name }}
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-tabs>
+
     <v-card-item>
       <v-window v-model="page">
-        <v-window-item value="flight">
-          <FlightAction></FlightAction>
-        </v-window-item>
-        <v-window-item value="weather">
-          <WeatherAction></WeatherAction>
-        </v-window-item>
-        <v-window-item value="fuel">
-          <FuelAction></FuelAction>
-        </v-window-item>
-        <v-window-item value="risk">
-          <RiskAction></RiskAction>
-        </v-window-item>
-        <v-window-item value="note">
-          <NoteAction></NoteAction>
+        <v-window-item
+          v-for="page in pages.pages.concat(pages.morePages)"
+          :key="page.shortName"
+          :value="page.shortName"
+        >
+          <component :is="page.component" />
         </v-window-item>
       </v-window>
     </v-card-item>
--- src/scripts/prep/actionpage.ts
+++ src/scripts/prep/actionpage.ts
@@ -1,6 +1,65 @@
-import { ref } from 'vue'
+import { ref, type Component } from 'vue'
+
+import FlightActionVue from '@/components/prep/actions/FlightAction.vue'
+import FuelActionVue from '@/components/prep/actions/FuelAction.vue'
+import WeightBalanceActionVue from '@/components/prep/actions/WeightBalanceAction.vue'
+import RiskActionVue from '@/components/prep/actions/RiskAction.vue'
+import NoteActionVue from '@/components/prep/actions/NoteAction.vue'
+import WeatherActionVue from '@/components/prep/actions/WeatherAction.vue'
 const page = ref()
 
+const pages: Page[] = [
+  {
+    name: 'Flight',
+    shortName: 'flight',
+    icon: 'mdi-airplane',
+    component: FlightActionVue,
+  },
+  {
+    name: 'Fuel',
+    shortName: 'fuel',
+    icon: 'mdi-gas-station',
+    component: FuelActionVue,
+  },
+  {
+    name: 'Weight & Balance',
+    shortName: 'weight',
+    icon: 'mdi-balance-scale',
+    component: WeightBalanceActionVue,
+  },
+  {
+    name: 'Risk Assment',
+    shortName: 'risk',
+    icon: 'mdi-order-bool-ascending-variant',
+    component: RiskActionVue,
+  },
+]
+
+const morePages: Page[] = [
+  {
+    name: 'Notes',
+    shortName: 'notes',
+    icon: 'mdi-note-text',
+    component: NoteActionVue,
+  },
+  {
+    name: 'Weather',
+    shortName: 'weather',
+    icon: 'mdi-weather-cloudy',
+    component: WeatherActionVue,
+  },
+]
+
+export const getPages = () => {
+  return { pages, morePages }
+}
 export const getPage = () => {
   return page
+}
+
+export interface Page {
+  name: string
+  shortName: string
+  icon?: string
+  component: Component
 }
